ecbabe57e46145c687a6772e41f5d3e132debc68
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-08-28 13:40:51 +0100

    Add page header

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,35 @@
 import { Fragment } from 'react'
+import { Container } from 'reactstrap'
+import Navbar from './Navbar'
+// import SvgLoader from 'bv-react-svgloader'
 
-export props => <Fragment>
-  {props.children}
-</Fragment>
+export default props => (
+  <Fragment>
+  <Container>
+    <Navbar/>
+  </Container>
+    <div className='bg-home page-header'></div>
+    {/* <SvgLoader src='/static/images/home-background.svg'/> */}
+    {props.children}
+    <style jsx>{`
+      .bg-home {
+        background-image: url(/static/images/home-background.svg);
+        height: 75vh;
+        width: 100%;
+      }
+      .page-header {
+        // height: 90vh;
+        margin: 0;
+        border: 0;
+        display: flex;
+        padding: 0;
+        overflow: hidden;
+        position: relative;
+        // max-height: 1000px;
+        align-items: center;
+        background-size: cover;
+        background-position: center center;
+      }
+    `}</style>
+  </Fragment>
+)
